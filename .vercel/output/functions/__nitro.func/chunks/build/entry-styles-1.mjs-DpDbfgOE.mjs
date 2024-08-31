ef03530cbd3bc9f45c0928761ca2220a2c657f2f
fangminL <17611595301@163.com>
2024-09-01 01:00:38 +0800

    init

---
+++ .vercel/output/functions/__nitro.func/chunks/build/entry-styles-1.mjs-DpDbfgOE.mjs
@@ -0,0 +1,4 @@
+const app_vue_vue_type_style_index_0_lang = ".page-enter-active,.page-leave-active{transition:all .4s}.page-enter-from,.page-leave-to{filter:blur(1rem);opacity:0}.rotate-enter-active,.rotate-leave-active{transition:all .4s}.rotate-enter-from,.rotate-leave-to{opacity:0;transform:rotate3d(1,1,1,15deg)}";
+
+export { app_vue_vue_type_style_index_0_lang as a };
+//# sourceMappingURL=entry-styles-1.mjs-DpDbfgOE.mjs.map
