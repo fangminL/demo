1de3ae0441fa958a9edd6e01f9a24e5a10c39c55
fangminL <17611595301@163.com>
2024-09-01 01:13:50 +0800

    init

--- .output/public/_nuxt/Cf58aPhk.js
+++
@@ -1,1 +0,0 @@
-import{u as s}from"./BKDBYnB6.js";import{v as a}from"./FDRnG0H-.js";let e=s();const o=()=>e.value.isLoggedIn,n=()=>{e.value.isLoggedIn=!0,e.value.name="张三",e.value.age=18,a("/users")},l=()=>{e.value.isLoggedIn=!1,e.value.name=null,e.value.age=null,a("/")};export{o as i,l as o,n as t};
